20b36cb5bd64f78a8d6a7d5fd7678bfffc592df5
Unknown <unknown@example.com>
2019-09-28 11:32:43 +0700

    Auto stash before merge of "master" and "origin/master"

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
+<<<<<<< Updated upstream
 	echo 'hello words';
+=======
+<<<<<<< HEAD
+	echo phpinfo();
+	echo 'Assalamualaikum';
+=======
+	echo 'hello words';
+>>>>>>> master
+>>>>>>> Stashed changes
 ?>
